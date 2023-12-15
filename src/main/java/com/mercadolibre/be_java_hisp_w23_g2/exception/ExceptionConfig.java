d1c82373133d97a20da9a639e7cd17fae9163df2
felipecosta03 <felipe.costa@mercadolibre.com>
2023-12-15 17:06:38 -0300

    NotFoundException created

--- src/main/java/com/mercadolibre/be_java_hisp_w23_g2/exception/ExceptionConfig.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w23_g2/exception/ExceptionConfig.java
@@ -1,7 +1,14 @@
 package com.mercadolibre.be_java_hisp_w23_g2.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionConfig {
+    @ExceptionHandler
+    public ResponseEntity<?> notFoundException(NotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
 }
