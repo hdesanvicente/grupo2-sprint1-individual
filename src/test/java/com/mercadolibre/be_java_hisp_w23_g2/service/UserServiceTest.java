c93aacde925780a7d3a49727f170dcd497c57fc1
cvargasmeli <claudia.vargassilva@mercadolibre.cl>
2023-12-27 16:17:03 -0300

    add T-0007 - US 02 - Followers count seller OK

--- src/test/java/com/mercadolibre/be_java_hisp_w23_g2/service/UserServiceTest.java
+++ src/test/java/com/mercadolibre/be_java_hisp_w23_g2/service/UserServiceTest.java
@@ -1,13 +1,24 @@
 package com.mercadolibre.be_java_hisp_w23_g2.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolibre.be_java_hisp_w23_g2.dto.UserFollowersCountDTO;
+import com.mercadolibre.be_java_hisp_w23_g2.entity.User;
+import com.mercadolibre.be_java_hisp_w23_g2.exception.NotFollowingException;
 import com.mercadolibre.be_java_hisp_w23_g2.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -17,8 +28,30 @@ class UserServiceTest {
     @InjectMocks
     UserService service;
 
+    @DisplayName("T-0007 - US 02 - Followers count seller OK")
     @Test
-    void getFollowersCountSeller() {
+    void getFollowersCountSellerTest() {
+        //Arrange
+        User user = new User();
+        user.setId(1);
+        user.setUserName("John Doe");
+
+        User follower1 = new User();
+        User follower2 = new User();
+
+        follower1.setId(2);
+        follower1.setUserName("Alice Smith");
+        follower2.setId(3);
+        follower2.setUserName("Bob Jones");
+
+        user.setFollowers(List.of(follower1, follower2));
+        when(repository.findUserById(user.getId())).thenReturn(user);
+
+        // Act
+        UserFollowersCountDTO expectedResult = service.getFollowersCountSeller(user.getId());
+
+        // Assert
+        Assertions.assertEquals(user.getFollowers().size(), expectedResult.getFollowersCount());
     }
 
     @Test
